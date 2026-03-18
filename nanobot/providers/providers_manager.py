3d20f4207d9024a173b0359bb6bb6dd30726f89b
Archermmt <archermmt@126.com>
2026-03-18 13:52:23 +0800

    testing

--- nanobot/providers/providers_manager.py
+++ nanobot/providers/providers_manager.py
@@ -83,7 +83,7 @@ class ProvidersManager:
 
         mode = mode or self._default_mode
         decide_mode = mode
-        user_content = messages[-1]["content"]
+        user_content = messages[-1]["content"].split("\n\n")[1]
         if mode == "auto":
             assert "main" in self._modes, "No main mode configured for auto mode"
             # For auto mode, use decider to choose the best mode
